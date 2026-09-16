eb3c788b2d0c85c08fd6c0ac4ab9ee82e6a1ce01
sunset00x <pratikkoirala444@gmail.com>
2026-09-16 08:10:05 +0545

    slight function behaviour

--- ajax/get-time-slots.php
+++ ajax/get-time-slots.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions.php';
 
-header('Content-Type: application/json');
+
 
 $doctorId = isset($_GET['doctor_id']) ? (int)$_GET['doctor_id'] : 0;
 $date     = isset($_GET['date']) ? sanitize($_GET['date']) : '';
